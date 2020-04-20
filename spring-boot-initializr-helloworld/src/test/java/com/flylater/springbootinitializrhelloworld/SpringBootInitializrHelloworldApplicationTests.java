3c8f8bfdbb741fa64b69cfc07936d16d90a794c2
cqrd_rszhang <rszhang@travelsky.com>
2020-04-20 11:29:54 +0800

    Spring Initializr快速构建Spring Boot

---
+++ spring-boot-initializr-helloworld/src/test/java/com/flylater/springbootinitializrhelloworld/SpringBootInitializrHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flylater.springbootinitializrhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInitializrHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
