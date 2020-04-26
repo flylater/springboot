5b215579befe4b69fb5c1974c109606ef5f049c6
flylater <flylater@126.com>
2020-04-26 17:16:19 +0800

    thymeleaf与webjars相结合

---
+++ springboot-thymeleaf-webjars/src/main/java/com/flylater/springboot/SpringbootThymeleafWebjarsApplication.java
@@ -0,0 +1,13 @@
+package com.flylater.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafWebjarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafWebjarsApplication.class, args);
+    }
+
+}
